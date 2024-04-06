b7d118d2d89174ea11bd37d2331f5ff778da63f4
hd-name <hamartamatoz25@gmail.com>
2024-04-06 16:24:02 +0200

    filter added to moview

--- MovieApp/movieapp.js
+++ MovieApp/movieapp.js
@@ -1,4 +1,4 @@
-const API_URL =
+const SEARCH_URL =
   "https://api.themoviedb.org/3/search/movie?api_key=662f494189bcfc373683799e0a162088&query=";
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 
@@ -12,9 +12,9 @@ form.addEventListener("submit", async (e) => {
   const searchTerm = search.value;
 
   if (searchTerm && searchTerm !== "") {
-    const movies = await getMovies(API_URL + searchTerm);
+    const movies = await getMovies(SEARCH_URL + searchTerm);
     const sortedMovies = sortMoviesByRating(movies);
-    showMovies(sortedMovies);
+    showSearchedMovies(sortedMovies);
 
     search.value = "";
   } else {
@@ -30,7 +30,7 @@ async function getMovies(url) {
   return data.results;
 }
 
-function showMovies(movies) {
+function showSearchedMovies(movies) {
   main.innerHTML = "";
 
   movies.forEach((movie) => {
@@ -46,7 +46,7 @@ function showMovies(movies) {
         <span class="${getClassByRate(vote_average)}">${vote_average}</span>
       </div>
       <div class="overview">
-        <h3>${title} <small>Overview</small></h3>
+        <h3>Overview</h3>
         <p>${overview}</p>
       </div>
     `;
@@ -67,3 +67,95 @@ function getClassByRate(vote) {
     return "red";
   }
 }
+document.addEventListener("DOMContentLoaded", function () {
+  const topRatedLink = document.getElementById("top_rated");
+  const popularLink = document.getElementById("popular");
+  const nowPlayingLink = document.getElementById("now_playing");
+  const upcoming = document.getElementById("upcoming");
+  fetchMovies("upcoming");
+
+  topRatedLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    fetchMovies("top_rated");
+  });
+
+  popularLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    fetchMovies("popular");
+  });
+  nowPlayingLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    fetchMovies("now_playing");
+  });
+  upcoming.addEventListener("click", function (event) {
+    event.preventDefault();
+    fetchMovies("upcoming");
+  });
+
+  function fetchMovies(type) {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NjJmNDk0MTg5YmNmYzM3MzY4Mzc5OWUwYTE2MjA4OCIsInN1YiI6IjY2MGVhOWVmZTAzOWYxMDE2MmU2Nzg4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.StlA3Bu4KquXnJc6tpvuMQbe6cBh3RG8P6A-CVKsXQY",
+      },
+    };
+
+    fetch(
+      `https://api.themoviedb.org/3/movie/${type}?language=en-US&page=1`, //parameter = type
+      options
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        showFilteredMovies(data.results);
+      })
+      .catch((err) => console.error(err));
+  }
+
+  function showFilteredMovies(movies) {
+    const main = document.getElementById("main");
+    main.innerHTML = ""; // clear films before a an action
+
+    // show function for films
+    movies.forEach((movie) => {
+      const movieDiv = document.createElement("div");
+      movieDiv.classList.add("movie");
+
+      const img = document.createElement("img");
+      img.src = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+      img.alt = `${movie.title} Poster`;
+
+      const movieInfo = document.createElement("div");
+      movieInfo.classList.add("movie-info");
+
+      const h3 = document.createElement("h3");
+      h3.textContent = movie.title;
+
+      const span = document.createElement("span");
+      span.classList.add("green");
+      span.textContent = movie.vote_average;
+
+      const overview = document.createElement("div");
+      overview.classList.add("overview");
+
+      const overviewH3 = document.createElement("h3");
+      overviewH3.textContent = "Overview";
+
+      const p = document.createElement("p");
+      p.textContent = movie.overview;
+
+      overview.appendChild(overviewH3);
+      overview.appendChild(p);
+
+      movieInfo.appendChild(h3);
+      movieInfo.appendChild(span);
+
+      movieDiv.appendChild(img);
+      movieDiv.appendChild(movieInfo);
+      movieDiv.appendChild(overview);
+
+      main.appendChild(movieDiv);
+    });
+  }
+});
